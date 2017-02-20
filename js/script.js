98fe9d60a6285ade726e2cb9bba483e824ced2dc
Goory al Hamed <gooryalhamed@hotmail.com>
2017-02-20 10:26:48 +0300

    implement preventDefault

--- js/script.js
+++ js/script.js
@@ -1,24 +1,13 @@
 $(document).ready(function(){
-  
-  // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
+
+    $('form').on('submit', function(event){
+    $("#hello").text("Hello, " + $('#name').val());
+
+     event.preventDefault();
+   });
+
   });
 
-  //examine event object
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   debugger;
-  //   event.preventDefault();
-  // });
+ 
 
-  // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
-
-});
+ 
